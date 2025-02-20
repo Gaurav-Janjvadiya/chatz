e4c76314a270efa744fc9c4a12705c3185cddc53
Gaurav-Janjvadiya <janjvadiyagaurav344@gmail.com>
2025-02-21 00:13:58 +0530

    Added common start script for server and client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import ChatListPanel from "./components/ChatListPanel";
 
 export default function App() {
   return (
-    <div className="bg-black">
+    <div className="bg-black grid grid-rows-1">
       <ChatListPanel />
     </div>
   );
